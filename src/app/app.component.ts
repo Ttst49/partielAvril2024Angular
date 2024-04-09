eea2d008a7da721f70cca33cc8df8c5c975c9bf9
Ttst <thibautstachnick@gmail.com>
2024-04-09 11:35:33 +0200

    change default component and add to it a bottom menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {initFlowbite} from "flowbite";
+import {HomeComponent} from "./home/home.component";
+import {Router} from "@angular/router";
+import {GlobalConstants} from "./common/GlobalConstants";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'partielAvril2024';
+
+  constructor(private router:Router) {
+  }
+
+  btnClick(name:string){
+    this.router.navigateByUrl("/"+name).then((r)=>{console.log("clicked")})
+  }
+
+  ngOnInit(){
+    initFlowbite()
+  }
+
+  protected readonly Router = Router;
+  protected readonly GlobalConstants = GlobalConstants;
 }
+
