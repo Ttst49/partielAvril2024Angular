2ba5ece909d6b3cd7fbccc9f4377de954dfb9a95
Ttst <thibautstachnick@gmail.com>
2024-04-09 11:35:48 +0200

    add all routes for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CartComponent} from "./cart/cart.component";
+import {ScanComponent} from "./scan/scan.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {UserManagerComponent} from "./user-manager/user-manager.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "cart", component: CartComponent
+  },
+  {
+    path: "scan", component: ScanComponent,
+
+  },
+  {
+    path: "login", component: UserManagerComponent
+  },
+  {
+    path: "logout", component: UserManagerComponent
+  },
+  {
+    path: "register", component: UserManagerComponent
+  },
+  {
+    path: "profile", component: ProfileComponent
+  }
+];
