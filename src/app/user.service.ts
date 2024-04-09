b251a8d8e5242be6a85f9c973b75ebb29356bc1e
Ttst <thibautstachnick@gmail.com>
2024-04-09 15:18:07 +0200

    add some way to login and register directly from app

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {GlobalConstants} from "./common/GlobalConstants";
 import {User} from "./user";
+import {Router} from "@angular/router";
+import {routes} from "./app.routes";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private router:Router) { }
 
 
   registerUser(user:User){
@@ -18,16 +20,23 @@ export class UserService {
           console.log(dataFromFetch);
         }
       })
+    this.router.navigateByUrl("login").then()
   }
 
   loginUser(user:User){
-    this.http.post(GlobalConstants.baseUrl+"login_check",user)
+    this.http.post(GlobalConstants.baseUrl+"login_check",
+      {
+        "username":user.username,
+        "password":user.password
+          }
+    )
       .subscribe({
-        next(dataFromFetch){
-          localStorage.setItem('bearerToken',dataFromFetch["token"])
+        next(dataFromFetch:any){
+          localStorage.setItem('bearerToken',dataFromFetch['token'])
           console.log(localStorage)
         }
       })
+    this.router.navigateByUrl("home").then()
   }
 
 }
