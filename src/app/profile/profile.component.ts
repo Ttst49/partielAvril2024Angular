4167c12a29f326d7530c26198789f96df24e14e5
Ttst <thibautstachnick@gmail.com>
2024-04-09 11:38:38 +0200

    add home component and it template

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {GlobalConstants} from "../common/GlobalConstants";
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+  constructor(private router:Router) {
+    if (!GlobalConstants.isLoggedIn()){
+      router.navigateByUrl("login").then()
+    }
+  }
+}
