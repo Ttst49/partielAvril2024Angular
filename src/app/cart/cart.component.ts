da074cb4824faf335a16a08bd9d8128c6f9bc9aa
Ttst <thibautstachnick@gmail.com>
2024-04-10 09:32:10 +0200

    update cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {GlobalConstants} from "../common/GlobalConstants";
 import {Router} from "@angular/router";
+import {CartService} from "../cart.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 
+  cartService:CartService = inject(CartService)
+
   constructor(private router:Router) {
     if (!GlobalConstants.isLoggedIn()){
       router.navigateByUrl("login").then()
     }
+    this.cartService.getCart()
   }
+
+  protected readonly GlobalConstants = GlobalConstants;
+  protected readonly Math = Math;
 }
