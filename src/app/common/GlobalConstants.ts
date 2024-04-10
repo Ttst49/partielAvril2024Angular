579fa611e0439c443c104465833dd0b7edd43b59
Ttst <thibautstachnick@gmail.com>
2024-04-10 09:31:59 +0200

    add cart to global Constants

--- src/app/common/GlobalConstants.ts
+++ src/app/common/GlobalConstants.ts
@@ -1,9 +1,22 @@
+import {FullProduct} from "../full-product";
+
 export class GlobalConstants{
 
   public static baseUrl = "http://localhost:8000/api/"
   public static token = localStorage.getItem("bearerToken")
+  public static user:any
 
   public static isLoggedIn(){
     return localStorage.getItem("bearerToken") != null
+  }
+  public static cart: FullProduct[] = []
+
+
+  public static getTotalPrice(){
+    let totalPrice = 0
+    this.cart.forEach((product:any)=>{
+      totalPrice += product.product.price*product.quantity
+    })
+    return totalPrice
   }
 }
